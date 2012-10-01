aa0c0f4ae0342093748a111e04641334061cb882
Peter Hallam <pragmatic@cpan.org>
2012-10-01 13:42:28 +0800

    First commit of Dist-Zilla-Plugin-PodSimpleWiki

---
+++ lib/Dist/Zilla/Plugin/PodSimpleWiki.pm
@@ -0,0 +1,31 @@
+#  COPYRIGHT: © 2012 Peter Hallam
+
+package Dist::Zilla::Plugin::PodSimpleWiki;
+
+#   ABSTRACT: This module does something
+#    CREATED: Mon, 1 Oct 2012 05:42:28 UTC
+#     AUTHOR: Peter Hallam <pragmatic@cpan.org>
+
+use strict;
+use warnings;
+use v5.10;
+
+# VERSION
+
+
+
+1;
+
+__END__
+
+=for Pod::Coverage
+
+=head1 SYNOPSIS
+
+ use Dist::Zilla::Plugin::PodSimpleWiki;
+
+ my $podsimplewiki = Dist::Zilla::Plugin::PodSimpleWiki->new;
+
+=head1 DESCRIPTION
+
+=cut
